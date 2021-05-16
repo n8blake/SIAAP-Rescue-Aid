858d55172cd2a008e66330b3e9e2edf767a5048c
Nate <n8blake@mac.com>
2021-05-16 15:42:10 -0400

    Update user routes, build login form on main page

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,20 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-  const user_name = document.querySelector('#email-login').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (user_name && password) {
+  if (email && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ user_name, password }),
+      body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
       document.location.replace('/');
     } else {
+      console.log(response.message);
       alert('Failed to log in');
     }
   }
